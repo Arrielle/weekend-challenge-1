82c88ff788b573e94008689945dfd676f92a6f42
Arrielle <arrielle@Arrielles-MBP.hsd1.mn.comcast.net>
2017-02-13 21:18:48 -0600

    moving functions outside of doc.ready

--- client.js
+++ client.js
@@ -1,3 +1,19 @@
+function Employee(inFirstName, inLastName, inIDNumber, inJobTitle, inAnnualSalary) {
+  this.firstName = inFirstName,
+  this.lastName = inLastName,
+  this.idNumber = inIDNumber,
+  this.jobTitle = inJobTitle,
+  this.annualSalary = parseInt(inAnnualSalary)
+};
+
+function calculateMonthlyExpenditure(){
+  var allEmployeeSalary = 0;
+  for (var i = 0; i < employeeArray.length; i++) {
+    allEmployeeSalary += employeeArray[i].annualSalary;
+  }
+  allEmployeeSalary = allEmployeeSalary / 12;
+  return allEmployeeSalary;
+};
 
 $(document).ready(function() {
   //An empty array.
@@ -5,118 +21,88 @@ $(document).ready(function() {
   //Creating the on.click function.
   $('#formButton').on('click', function(){
     //Store my inputs into a variable for later use
-    var firstName = $('#firstName').val();
-    var lastName = $('#lastName').val();
-    var idNumber = $('#idNumber').val();
-    var jobTitle = $('#jobTitle').val();
+    var firstName = $('#firstName').val().toUpperCase();
+    var lastName = $('#lastName').val().toUpperCase();
+    var idNumber = $('#idNumber').val().toUpperCase();
+    var jobTitle = $('#jobTitle').val().toUpperCase();
     var annualSalary = $('#annualSalary').val();
+
+    //if statement checks to see if the value entered for annualSalary was a number.
+    //if NOT a number then an alert message shows below the form.
+    //if it IS a number, my initial code is run.
+    if (!$.isNumeric(annualSalary)) {
+      $('#alertMessage').text('The salary you entered was not a number. Please try again.')
+    } else {
     //Adds new employee data to the table on submit.
-    $('#employeeTable').append('<tr><th>' + firstName + '</th><th>' +
-    lastName + '</th><th>' + idNumber + '</th><th>' + jobTitle + '</th><th>' +
+    $('#employeeTable').append('<tr><td>' + firstName + '</td><td>' +
+    lastName + '</td><td>' + idNumber + '</td><td>' + jobTitle + '</td><th>' +
     annualSalary + '</th><th><button type="button" class = "deleteButton" name="button">Delete</button></th></tr>');
     //object constructor
-    function Employee(inFirstName, inLastName, inIDNumber, inJobTitle, inAnnualSalary) {
-      this.firstName = firstName,
-      this.lastName = lastName,
-      this.idNumber = idNumber,
-      this.jobTitle = jobTitle,
-      this.annualSalary = annualSalary
-    }
+
     // //Creates an object from the input data
     var employeeData = new Employee(firstName, lastName, idNumber, jobTitle, annualSalary)
     //pushes object onto the array.
     employeeArray.push(employeeData);
+    //making a function to find total salary amount paid by the company each month.
+    var monthlyExpenditureOutput = calculateMonthlyExpenditure();
+//ends my salary function
+    //Now, I need to add the result of my salary function to the DOM
+    $('#totalExpenditure').children().children().last().text('Total Monthly Expenditure: ' + monthlyExpenditureOutput);
+    //resets from once it's been submitted
+    document.forms['myForm'].reset();
+    $('#alertMessage').text('');
+  };//ends the if else
   }); //ends onclick submit event listener
-
-  //submit onclick listener end
-
-
   //Removes employee data from the table if the delete button is pressed.
   $('#employeeTable').on('click', '.deleteButton', function(){
-    $(this).parent().parent().remove();
+    //creating a row variable
+    var row = $(this).parent().parent();
+    //finding the row index # so I can find the array #
+    var arrayIndex = $(this).parent().parent().index('tr') - 1;
+    //removing the row from the DOM
+    $(row).remove();
+    //splicing the object from the arrayIndex
+    employeeArray.splice(arrayIndex, 1);
+    //making a function to recalculate the total salary amount paid by the company each month.
+    var monthlyExpenditureOutput = calculateMonthlyExpenditure();
+    $('#totalExpenditure').children().children().last().text('Total Monthly Expenditure: ' + monthlyExpenditureOutput);
 
-    //find out which index has been chosen for deletion
-    //remove the item.
+
   }); //ends onclick delete button event listener
 }); //ends doc.ready event listener
 
 
 
-
-
-  //set and empty array?
-//
-//
-//   //make an empty shell object
-//   var employeeArray [];
-//   var employeeShell {};
-//
-//   //Make the new object that is based on the employee shell. This new object takes in the information
-//   //from a PARTICULAR INPUT and stores it.
-//
-//   function Employee(firstName, lastName, idNumber, jobTitle, annualSalary){
-//     this.firstName = firstName,
-//     this.lastName = lastName,
-//     this.idNumber = idNumber,
-//     this.jobTitle = jobTitle,
-//     this.annualSalary = annualSalary,
-//   });
-//
-//   var employeeShell = new Employee(this.firstName, this.lastName, this.idNumber, this.jobTitle, this.annualSalary);
-//
-//   for (var i = 0; i < array.length; i++) {
-//     array[i]
-//   }
-//
-// //pushing the array
-// // employeeArray.push(Employee);
-//
-//
-// //   //Store input into an object
-// //   //var employeeObject = {}; //creating an empty object to store data in
-// //   //how to push input to this object?
-//
-// //   function formsToObj(){
-// //       var forms = [];
-// //       $('employeeForm').each(function(i){
-// //           forms[i] = {};
-// //           $(this).children('input,textarea,select').each(function(){
-// //               forms[i][$(this).attr('name')] = $(this).val();
-// //           });
-// //       });
-// //       return forms;
-// //   }
-// //   //
-//
-// //   //
-// //   //   var employeeArray = []; //creating an empty Array
-// //   //
-// //
-// //
-// // $()
-// //
-// // });
-// //
-// //
-// // Get data from input,
-// // Store input in an object,
-// // Store object in an array,
-// // Push object to Employee List,
-// //
-// // write a for loop to go through the array and add
-// // annualSalary/12.
-// //
-// // //create a function that passes an array index
-// //
-// empty array
-//
 // has submit been pressed?
 // if yes
 // Create new object based on input
 // push object onto array
-// end function
-//
+// determine the total amount spent by the company based on the employees still in the array
+// take that number / 12
+// add the final number to the DOM
+
 // has delete been pressed
 // if yes
 // delete selected item from the DOM
-// delete selected item from array
+//dom traversal
+// delete selected item from array (splice)
+//find out which array index you are on?!
+//determine the total amount spent by the company based on the employees still in the array
+// take that number / 12
+// add the final number to the DOM
+
+
+
+//need to make a function that adds all of the arrays annualSalary/12 onto the end of monthlySalaryExpenditure div.
+//needs to be checked when a new employee is added
+//needs to be checked when an employee is removed
+
+//need to put the result from the last step into the DOM
+//needs to happen on new employee addition
+//needs to happen on employee removal
+
+
+
+//extra functionality
+//IF the annualSalary input is not a number,
+//alert with an error and do not submit.
